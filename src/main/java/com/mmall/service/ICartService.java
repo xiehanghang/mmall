309ca98c2f10ce5058137ea154fb32627bd2ec0a
xiehang <1309049396@qq.com>
2019-01-15 15:42:26 +0800

    完成购物车模块和收货地址模块的开发测试，完成Linux的环境配置

---
+++ src/main/java/com/mmall/service/ICartService.java
@@ -0,0 +1,16 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.vo.CartVo;
+
+/**
+ * Created by Enzo Cotter on 2019/1/14.
+ */
+public interface ICartService {
+    ServerResponse<CartVo> add(Integer userId, Integer productId, Integer count);
+    ServerResponse<CartVo> update(Integer userId, Integer productId, Integer count);
+    ServerResponse<CartVo> deleteProduct(Integer userId, String productIds);
+    ServerResponse<CartVo> list(Integer userId);
+    ServerResponse<CartVo> selectOrUnSelect(Integer userId, Integer productId, Integer checked);
+    ServerResponse<Integer> getCartProductCount(Integer userId);
+}
