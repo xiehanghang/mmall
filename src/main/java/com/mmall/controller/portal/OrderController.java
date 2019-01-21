637f4dd388839b5cd0c4e4e790531ab451f865b1
xiehang <1309049396@qq.com>
2019-01-21 20:28:21 +0800

    æ抽空把支付å®åç»功能完善了,项目1.0算æ³是完工了

--- src/main/java/com/mmall/controller/portal/OrderController.java
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -12,7 +12,6 @@ import com.mmall.service.IOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,66 +23,29 @@ import java.util.Iterator;
 import java.util.Map;
 
 /**
- * Created by Enzo Cotter on 2019/1/16.
+ * Created by geely
  */
+
 @Controller
 @RequestMapping("/order/")
 public class OrderController {
 
-    @Autowired
-    private IOrderService iOrderService;
-    @Autowired
     private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
+    @Autowired
+    private IOrderService iOrderService;
 
-    /**
-     * 分页-订单列表
-     * @param session
-     * @param pageNum
-     * @param pageSize
-     * @return
-     */
-    @RequestMapping("list.do")
+
+    @RequestMapping("create.do")
     @ResponseBody
-    public ServerResponse list(HttpSession session, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
-                               @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+    public ServerResponse create(HttpSession session, Integer shippingId) {
         User user = (User) session.getAttribute(Const.CURRENT_USER);
         if (user == null) {
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
         }
-        return iOrderService.getOrderList(user.getId(), pageNum, pageSize);
+        return iOrderService.create(user.getId(), shippingId);
     }
 
-    /**
-     * 查询订单详情
-     * @param session
-     * @param orderNo
-     * @return
-     */
-    @RequestMapping("detail.do")
-    @ResponseBody
-    public ServerResponse detail(HttpSession session, Long orderNo) {
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
-        if (user == null) {
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
-        }
-        return iOrderService.getOrderDetail(user.getId(), orderNo);
-    }
-
-    /**
-     * 获取购物车中已经选中的商品
-     * @param session
-     * @return
-     */
-    @RequestMapping("get_order_cart_product.do")
-    @ResponseBody
-    public ServerResponse getOrderCartProduct(HttpSession session) {
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
-        if (user == null) {
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
-        }
-        return iOrderService.getOrderCartProduct(user.getId());
-    }
 
     @RequestMapping("cancel.do")
     @ResponseBody
@@ -95,29 +57,39 @@ public class OrderController {
         return iOrderService.cancel(user.getId(), orderNo);
     }
 
-    /**
-     * 创建订单
-     * @param session
-     * @param shippingId
-     * @return
-     */
-    @RequestMapping("create.do")
+
+    @RequestMapping("get_order_cart_product.do")
     @ResponseBody
-    public ServerResponse create(HttpSession session, Integer shippingId) {
+    public ServerResponse getOrderCartProduct(HttpSession session) {
         User user = (User) session.getAttribute(Const.CURRENT_USER);
         if (user == null) {
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
         }
-        return iOrderService.create(user.getId(), shippingId);
+        return iOrderService.getOrderCartProduct(user.getId());
     }
 
-    /**
-     * 支付宝支付接口
-     * @param session
-     * @param orderNo
-     * @param request
-     * @return
-     */
+
+    @RequestMapping("detail.do")
+    @ResponseBody
+    public ServerResponse detail(HttpSession session, Long orderNo) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iOrderService.getOrderDetail(user.getId(), orderNo);
+    }
+
+    @RequestMapping("list.do")
+    @ResponseBody
+    public ServerResponse list(HttpSession session, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum, @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iOrderService.getOrderList(user.getId(), pageNum, pageSize);
+    }
+
+
     @RequestMapping("pay.do")
     @ResponseBody
     public ServerResponse pay(HttpSession session, Long orderNo, HttpServletRequest request) {
@@ -129,6 +101,49 @@ public class OrderController {
         return iOrderService.pay(orderNo, user.getId(), path);
     }
 
+    @RequestMapping("alipay_callback.do")
+    @ResponseBody
+    public Object alipayCallback(HttpServletRequest request) {
+        Map<String, String> params = Maps.newHashMap();
+
+        Map requestParams = request.getParameterMap();
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext(); ) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            params.put(name, valueStr);
+        }
+        logger.info("支付宝回调,sign:{},trade_status:{},参数:{}", params.get("sign"), params.get("trade_status"), params.toString());
+
+        //非常重要,验证回调的正确性,是不是支付宝发的.并且呢还要避免重复通知.
+
+        params.remove("sign_type");
+        try {
+            boolean alipayRSACheckedV2 = AlipaySignature.rsaCheckV2(params, Configs.getAlipayPublicKey(), "utf-8", Configs.getSignType());
+
+            if (!alipayRSACheckedV2) {
+                return ServerResponse.createByErrorMessage("非法请求,验证不通过,再恶意请求我就报警找网警了");
+            }
+        } catch (AlipayApiException e) {
+            logger.error("支付宝验证回调异常", e);
+        }
+
+        //todo 验证各种数据
+
+
+        //
+        ServerResponse serverResponse = iOrderService.aliCallback(params);
+        if (serverResponse.isSuccess()) {
+            return Const.AlipayCallback.RESPONSE_SUCCESS;
+        }
+        return Const.AlipayCallback.RESPONSE_FAILED;
+    }
+
+
     @RequestMapping("query_order_pay_status.do")
     @ResponseBody
     public ServerResponse<Boolean> queryOrderPayStatus(HttpSession session, Long orderNo) {
@@ -136,55 +151,13 @@ public class OrderController {
         if (user == null) {
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
         }
-        ServerResponse serverResponse =  iOrderService.queryOrderPayStatus(user.getId(), orderNo);
+
+        ServerResponse serverResponse = iOrderService.queryOrderPayStatus(user.getId(), orderNo);
         if (serverResponse.isSuccess()) {
-            return serverResponse.createBySuccess(true);
+            return ServerResponse.createBySuccess(true);
         }
-        return serverResponse.createBySuccess(false);
+        return ServerResponse.createBySuccess(false);
     }
 
-    /**
-     * 支付宝回调接口
-     *
-     * @param request
-     * @return
-     */
-    @RequestMapping("alipy_callback.do")
-    @ResponseBody
-    public Object alipayCallback(HttpServletRequest request) {
-        Map<String, String> params = Maps.newHashMap();
-
-        Map requestParams = request.getParameterMap();
-        for (Iterator iterator = requestParams.keySet().iterator(); iterator.hasNext(); ) {
-            String name = (String) iterator.next();
-            String[] values = (String[]) requestParams.get(name);
-            String valueStr = "";
-            for (int i = 0; i < values.length; i++) {
-                valueStr = i == values.length - 1 ? valueStr + values[i] : valueStr + values[i] + ",";
-            }
-            params.put(name, valueStr);
-        }
-        //todo 19.1.17 16:30 去打网球,18：37开始
-        logger.info("支付宝回调，sign:{},trade_status:{}，参数:{}", params.get("sign"), params.get("trade_status"), params.toString());
-
-        //非常重要，验证回调的正确性，是不是支付宝发的，并且还要避免重复通知
-        params.remove("sign_type");
-        try {
-            boolean alipayRSACheckedV2 = AlipaySignature.rsaCheckV2(params, Configs.getPublicKey(), "utf-8", Configs.getSignType());
-
-            if (!alipayRSACheckedV2) {
-                ServerResponse.createByErrorMessage("非法请求，验证不通过，再继续恶意请求就去见网警吧");
-            }
-        } catch (AlipayApiException e) {
-            logger.error("支付宝回调异常", e);
-        }
-        //todo 验证各种数据
-        //
-        ServerResponse serverResponse = iOrderService.aliCallback(params);
-        if (serverResponse.isSuccess()) {
-            return Const.AlipayCallback.TRADE_STATUS_TRADE_SUCCESS;
-        }
-        return Const.AlipayCallback.RESPONSE_FAILED;
-    }
 
 }
