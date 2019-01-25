a7b1942cf2a279cde31b474b053a48fe8359389e
xiehang <1309049396@qq.com>
2019-01-25 09:28:06 +0800

    cookie util

---
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -0,0 +1,57 @@
+package com.mmall.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class CookieUtil {
+
+    private final static String COOKIE_DOMAIN = ".happymmall.com";
+    private final static String COOKIE_NAME = "mmall_login_token";
+
+    public static String readLoginToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                log.info("read cookieName:{}, cookieValue:{}", cookie.getName(), cookie.getValue());
+                if (StringUtils.equals(cookie.getName(), COOKIE_NAME)) {
+                    log.info("return cookieName:{}, cookieValue:{}", cookie.getName(), cookie.getValue());
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void writeLoginToken(HttpServletResponse response, String token) {
+        Cookie cookie = new Cookie(COOKIE_NAME, token);
+        cookie.setDomain(COOKIE_DOMAIN);
+        cookie.setPath("/"); //代表设置在根目录
+
+        //单位是秒。
+        //如果这个maxage不设置的话，cookie就不会写入硬盘，而是写在内存。只在当前页面有效。
+        cookie.setMaxAge(60 * 60 * 24 * 365); //-1代表永久
+        log.info("write cookieName:{}, cookieValue:{}", cookie.getName(), cookie.getValue());
+        response.addCookie(cookie);
+    }
+
+    public static void delLoginToken(HttpServletRequest request,HttpServletResponse response){
+        Cookie[] cks = request.getCookies();
+        if(cks != null){
+            for(Cookie ck : cks){
+                if(org.apache.commons.lang.StringUtils.equals(ck.getName(),COOKIE_NAME)){
+                    ck.setDomain(COOKIE_DOMAIN);
+                    ck.setPath("/");
+                    ck.setMaxAge(0);//设置成0，代表删除此cookie。
+                    log.info("del cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
+                    response.addCookie(ck);
+                    return;
+                }
+            }
+        }
+    }
+}
