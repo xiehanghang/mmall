02bafd1ab325df9b48ec713364baffca73bac2c1
xiehang <1309049396@qq.com>
2019-01-26 20:34:35 +0800

    spring session

--- src/main/java/com/mmall/pojo/User.java
+++ src/main/java/com/mmall/pojo/User.java
@@ -5,13 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
     private Integer id;
 
     private String username;
