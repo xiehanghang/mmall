ea4843377ae5741c93397e48bbf7dfb95700a008
xiehang <1309049396@qq.com>
2019-01-29 20:04:37 +0800

    æSpingMVC拦截器实现统一校验ç

--- src/main/java/com/mmall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -34,76 +34,88 @@ public class CategoryManageController {
     @ResponseBody
     public ServerResponse addCategory(HttpServletRequest httpServletRequest, String categoryName,
                                       @RequestParam(value = "parentId", defaultValue = "0") Integer parentId) {
-        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
-        if (StringUtils.isEmpty(loginToken)) {
-            return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
-        }
-        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
-        User user = JsonUtil.string2Obj(userJsonStr, User.class);
-        //校验一下是否管理员
-        if (iUserService.checkAdminRole(user).isSuccess()) {
-            //是管理员
-            //增加处理分类逻辑
-            return iCategoryService.addCategory(categoryName, parentId);
-        } else {
-            return ServerResponse.createByErrorMessage("非管理员，无权限操作");
-        }
+//        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+//        if(StringUtils.isEmpty(loginToken)){
+//            return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
+//        }
+//        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+//        User user = JsonUtil.string2Obj(userJsonStr,User.class);
+//
+//        if(user == null){
+//            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录");
+//        }
+//        //校验一下是否是管理员
+//        if(iUserService.checkAdminRole(user).isSuccess()){
+//            //是管理员
+//            //增加我们处理分类的逻辑
+//            return iCategoryService.addCategory(categoryName,parentId);
+//
+//        }else{
+//            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+//        }
+        //全部通过拦截器验证是否登录以及权限
+        return iCategoryService.addCategory(categoryName,parentId);
     }
 
     @RequestMapping(value = "set_category_name.do", method = RequestMethod.GET)
     @ResponseBody
     public ServerResponse setCategoryName(HttpServletRequest httpServletRequest, String categoryName, Integer categoryId) {
-        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
-        if (StringUtils.isEmpty(loginToken)) {
-            return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
-        }
-        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
-        User user = JsonUtil.string2Obj(userJsonStr, User.class);
-        //校验一下是否管理员
-        if (iUserService.checkAdminRole(user).isSuccess()) {
-            //更新categoryName
-
-            return iCategoryService.updateCategryName(categoryName, categoryId);
-        } else {
-            return ServerResponse.createByErrorMessage("非管理员，无权限操作");
-        }
+//        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+//        if (StringUtils.isEmpty(loginToken)) {
+//            return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
+//        }
+//        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+//        User user = JsonUtil.string2Obj(userJsonStr, User.class);
+//        //校验一下是否管理员
+//        if (iUserService.checkAdminRole(user).isSuccess()) {
+//            //更新categoryName
+//            return iCategoryService.updateCategryName(categoryName, categoryId);
+//        } else {
+//            return ServerResponse.createByErrorMessage("非管理员，无权限操作");
+//        }
+        //全部通过拦截器验证是否登录以及权限
+        return iCategoryService.updateCategryName(categoryName, categoryId);
     }
 
     @RequestMapping(value = "get_category.do", method = RequestMethod.GET)
     @ResponseBody
     public ServerResponse getChildrenParallelCategory(HttpServletRequest httpServletRequest,
                                                       @RequestParam(value = "categoryId", defaultValue = "0") Integer categoryId){
-        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
-        if (StringUtils.isEmpty(loginToken)) {
-            return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
-        }
-        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
-        User user = JsonUtil.string2Obj(userJsonStr, User.class);
-        //校验一下是否管理员
-        if (iUserService.checkAdminRole(user).isSuccess()) {
-            //查询子节点的category信息，并且无递归，平级
-            return iCategoryService.getChildrenParallelCategory(categoryId);
-        } else {
-            return ServerResponse.createByErrorMessage("非管理员，无权限操作");
-        }
+//        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+//        if (StringUtils.isEmpty(loginToken)) {
+//            return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
+//        }
+//        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+//        User user = JsonUtil.string2Obj(userJsonStr, User.class);
+//        //校验一下是否管理员
+//        if (iUserService.checkAdminRole(user).isSuccess()) {
+//            //查询子节点的category信息，并且无递归，平级
+//            return iCategoryService.getChildrenParallelCategory(categoryId);
+//        } else {
+//            return ServerResponse.createByErrorMessage("非管理员，无权限操作");
+//        }
+        //全部通过拦截器验证是否登录以及权限
+        return iCategoryService.getChildrenParallelCategory(categoryId);
     }
 
     @RequestMapping(value = "get_deep_category.do", method = RequestMethod.GET)
     @ResponseBody
     public ServerResponse getCategoryAndDeepChildrenCategory(HttpServletRequest httpServletRequest,
                                                       @RequestParam(value = "categoryId", defaultValue = "0") Integer categoryId){
-        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
-        if (StringUtils.isEmpty(loginToken)) {
-            return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
-        }
-        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
-        User user = JsonUtil.string2Obj(userJsonStr, User.class);
-        //校验一下是否管理员
-        if (iUserService.checkAdminRole(user).isSuccess()) {
-            //查询当前节点 id 和递归子节点 id
-            return iCategoryService.selectCategoryAndChildrenById(categoryId);
-        } else {
-            return ServerResponse.createByErrorMessage("非管理员，无权限操作");
-        }
+//        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+//        if (StringUtils.isEmpty(loginToken)) {
+//            return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
+//        }
+//        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+//        User user = JsonUtil.string2Obj(userJsonStr, User.class);
+//        //校验一下是否管理员
+//        if (iUserService.checkAdminRole(user).isSuccess()) {
+//            //查询当前节点 id 和递归子节点 id
+//            return iCategoryService.selectCategoryAndChildrenById(categoryId);
+//        } else {
+//            return ServerResponse.createByErrorMessage("非管理员，无权限操作");
+//        }
+        return iCategoryService.selectCategoryAndChildrenById(categoryId);
+        //全部通过拦截器验证是否登录以及权限
     }
 }
--- src/main/java/com/mmall/controller/backend/UserManageController.java
+++ src/main/java/com/mmall/controller/backend/UserManageController.java
@@ -4,12 +4,16 @@ import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisShardedPoolUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -22,15 +26,21 @@ public class UserManageController {
     @Autowired
     private IUserService iUserService;
 
-    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @RequestMapping(value = "login.do", method = RequestMethod.GET)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse) {
         ServerResponse<User> response = iUserService.login(username, password);
         if (response.isSuccess()){
             User user = response.getData();
             if (user.getRole() == Const.Role.ROLE_ADMIN){
                 //说明登陆的是管理员
-                session.setAttribute(Const.CURRENT_USER, user);
+//                session.setAttribute(Const.CURRENT_USER, user);
+
+                //新增redie共享cookie，session的方式
+                CookieUtil.writeLoginToken(httpServletResponse, session.getId());
+                RedisShardedPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()),
+                        Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+
                 return response;
             } else {
                 return ServerResponse.createByErrorMessage("不是管理员，无法登陆");
