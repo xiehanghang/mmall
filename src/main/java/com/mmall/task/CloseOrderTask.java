473fba288928fa415cd39302f37e652144e11bb7
xiehang <1309049396@qq.com>
2019-01-30 18:28:29 +0800

    redis lock test1

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -44,7 +44,7 @@ public class CloseOrderTask {
         RedisShardedPoolUtil.setnx(lockName, "50");//有效期 50 秒，防止死锁
         log.info("获取{},ThreadName{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
         int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour", "2"));
-        iOrderService.closeOrder(hour);
+//        iOrderService.closeOrder(hour);
         RedisShardedPoolUtil.del(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
         log.info("释放{},ThreadName{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
         log.info("=============================================================");
